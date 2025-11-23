ecef97179a082a2a5b36eae104e69b1bc4d3b0e3
Eduardo-Rubel <ercanavarro@minha.fag.edu.br>
2025-11-23 19:18:42 -0300

    comit 13

--- Movie.java
+++ Movie.java
@@ -8,7 +8,7 @@
 
     class RegularPrice extends Price {
         int getPriceCode() { return Movie.REGULAR; }
-        double getCharge(int daysRented) {
+        public double getCharge(int daysRented) {
             double result = 2;
             if (daysRented > 2)
                 result += (daysRented - 2) * 1.5;
@@ -18,7 +18,7 @@
 
     class ChildrensPrice extends Price {
         int getPriceCode() { return Movie.CHILDRENS; }
-        double getCharge(int daysRented) {
+        public double getCharge(int daysRented) {
             double result = 1.5;
             if (daysRented > 3)
                 result += (daysRented - 3) * 1.5;
@@ -28,7 +28,7 @@
 
     class NewReleasePrice extends Price {
         int getPriceCode() { return Movie.NEW_RELEASE; }
-        double getCharge(int daysRented) {
+        public double getCharge(int daysRented) {
             return daysRented * 3;
         }
         int getFrequentRenterPoints(int daysRented) {
