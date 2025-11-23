564ad62be5d6b427cc81486f485e76089fc01026
Eduardo-Rubel <ercanavarro@minha.fag.edu.br>
2025-11-23 19:18:16 -0300

    comit 12

--- Movie.java
+++ Movie.java
@@ -1,6 +1,6 @@
     abstract class Price {
         abstract int getPriceCode();
-        abstract double getCharge(int daysRented);
+        public abstract double getCharge(int daysRented);
         int getFrequentRenterPoints(int daysRented) {
             return 1;
         }
