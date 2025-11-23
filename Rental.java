e18d2529335361aa6ee87ea090e971de32bf08b4
Eduardo-Rubel <ercanavarro@minha.fag.edu.br>
2025-11-23 19:13:07 -0300

    Comit 4

---
+++ Rental.java
@@ -0,0 +1,57 @@
+      public double getCharge() {
+         double result = 0;
+         switch (_movie.getPriceCode()) {
+            case Movie.REGULAR:
+               result += 2;
+               if (_daysRented > 2)
+                  result += (_daysRented - 2) * 1.5;
+               break;
+            case Movie.NEW_RELEASE:
+               result += _daysRented * 3;
+               break;
+            case Movie.CHILDRENS:
+               result += 1.5;
+               if (_daysRented > 3)
+                  result += (_daysRented - 3) * 1.5;
+               break;
+         }
+         return result;
+      }
+   public double getCharge() {
+      double thisAmount = 0;
+
+      switch (getMovie().getPriceCode()) {
+         case Movie.REGULAR:
+            thisAmount += 2;
+            if (getDaysRented() > 2)
+               thisAmount += (getDaysRented() - 2) * 1.5;
+            break;
+         case Movie.NEW_RELEASE:
+            thisAmount += getDaysRented() * 3;
+            break;
+         case Movie.CHILDRENS:
+            thisAmount += 1.5;
+            if (getDaysRented() > 3)
+               thisAmount += (getDaysRented() - 3) * 1.5;
+            break;
+      }
+      return thisAmount;
+   }
+public class Rental {
+
+   private Movie _movie;
+   private int _daysRented;
+
+   public Rental(Movie movie, int daysRented) {
+      _movie = movie;
+      _daysRented = daysRented;
+   }
+
+   public int getDaysRented() {
+      return _daysRented;
+   }
+
+   public Movie getMovie() {
+      return _movie;
+   }
+}
